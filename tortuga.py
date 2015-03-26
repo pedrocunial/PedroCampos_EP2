cf0102d353ea6f5c3ec151b1d813825b81b33002
pedrocunial <pedro_cunial@hotmail.com>
2015-03-26 15:27:24 -0300

    Tartaruga v4

--- tortuga.py
+++ tortuga.py
@@ -7,6 +7,7 @@ Created on Tue Mar 24 11:28:42 2015
 #Pedro Cunial Campos
 import random
 import turtle
+import tkinter
 
 dic = open('entrada.txt', encoding= 'utf-8')
 dic = dic.readlines()
@@ -33,26 +34,33 @@ t.setpos(-200,0)
 t.pendown()
 t.color("orange")
 t.shape("turtle")
-t.fd(50)
-t.left(90)
-t.fd(200)
-t.right(90)
-t.fd(50)
-t.right(90)
-t.fd(30)
-t.pu()
-t.fd(170)
-t.left(90)
 
-for i in range(len(p)):
-    if p[i] == ' ':
-        t.penup()
-        t.fd(13)
-    else:    
-        t.pendown()
-        t.fd(10)
-        t.penup()
-        t.fd(3)
+def forca():
+    t.fd(50)
+    t.left(90)
+    t.fd(200)
+    t.right(90)
+    t.fd(50)
+    t.right(90)
+    t.fd(30)
+    t.pu()
+    t.fd(170)
+    t.left(90)
+    
+forca()
+
+def linhas():
+    for i in range(len(p)):
+        if p[i] == ' ':
+            t.penup()
+            t.fd(13)
+        else:    
+            t.pendown()
+            t.fd(10)
+            t.penup()
+            t.fd(3)
+            
+linhas()
 
 t.hideturtle()
 
@@ -65,7 +73,7 @@ def cabeca():  #desenha cabeça
     t.pu
     t.hideturtle()
 
-def corpo():
+def corpo(): #desenha corpo (tronco)
     t.showturtle()
     t.pu()
     t.setpos(-100,150)
@@ -74,7 +82,7 @@ def corpo():
     t.pu()
     t.hideturtle()
 
-def braco_direito():
+def braco_direito(): #desenha braço direito
     t.showturtle()
     t.pu()
     t.setpos(-100,140)
@@ -83,7 +91,59 @@ def braco_direito():
     t.ht()
     t.pu()    
 
-def braco_esquerdo():
+def braco_esquerdo(): #desenha braço esquerdo
+    t.st()
+    t.pu()
+    t.setpos(-100,140)
+    t.pd()
+    t.setpos(-130,110)
+    t.ht()
+    t.pu()
+
+def perna_esquerda(): #desenha perna esquerda
+    t.st()
+    t.pu()
+    t.setpos(-100,120)
+    t.pd()
+    t.setpos(-120,75)
+    t.pu()
+    t.ht()    
     
+def perna_direita(): #desenha perna direita
+    t.st()
+    t.pu()
+    t.setpos(-100,120)
+    t.pd()
+    t.setpos(-80,75)
+    t.pu()
+    t.ht()
     
+jogo = True
+erro = 0
+
+while jogo == True:
+    j = window.textinput("Jogada", "Escolha uma letra. Mas escolha rápido, você não quer ver o Billy morto...") 
+    j = j.upper()    
+    if j in p:
+        ind = p.index(j)
+        print(ind)
+    else:
+        erro += 1
+        
+    if erro == 1:
+        cabeca()
+    elif erro == 2:
+        corpo()
+    elif erro == 3:
+        braco_esquerdo()
+    elif erro == 4:
+        braco_direito()
+    elif erro == 5:
+        perna_direita()
+    elif erro == 6:
+        perna_esquerda()
+        jogo == False
+        replay = tkinter.messagebox.askyesno('Game Over', 'Você perdeu, deseja jogar novamente?')
+        print(replay)
+        
 window.exitonclick()
